c9c527b5f238c28a033cfa6c29018831add27418
michaelgorman187 <michaelgorman187@gmail.com>
2025-04-17 14:31:35 +0100

    Update reacher.js

--- reacher.js
+++ reacher.js
@@ -10,6 +10,12 @@ fetch('reacher.json')
         const seriesHeader = document.createElement('h2');
         seriesHeader.textContent = `Series: ${seriesName}`;
         booksDiv.appendChild(seriesHeader);
+
+        data.books.forEach((book, index) => {
+            console.log(`Book ${index + 1}:`);
+            console.log(`Title: ${books.title}`);
+            console.log(`Pages: ${books.pages}`);
+            console.log('------------');
         
         });
 
